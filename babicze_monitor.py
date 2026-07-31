f52f14732720130f452c1fbbce8b996af01ba933
Dexter1479 <dexter1479@gmail.com>
2026-07-31 21:15:09 +0100

    Update babicze_monitor.py

--- babicze_monitor.py
+++ babicze_monitor.py
@@ -326,15 +326,6 @@ def compare(old, current):
 def main():
     print("Sprawdzam Babicze Croydon...")
 
-    send_email(
-        "TEST - BABICZE MONITOR",
-        [
-            "To jest test.",
-            "Jeśli widzisz tę wiadomość, powiadomienia działają poprawnie."
-        ]
-    )
-
-
     old = load_state()
     current = get_products()
 
